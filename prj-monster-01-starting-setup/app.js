93b95045d0e2e131c484eda6b893654087fee7e5
juuuuune <ilohe@nate.com>
2023-03-20 22:45:27 +0900

    53

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
     data(){
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0
         };
     },
     computed: {
@@ -15,11 +16,19 @@ const app = Vue.createApp({
         },
         playerBarStyle(){
             return {width: this.playerHealth+'%'};
+        },
+        mayUseSpecialAttack(){
+            if(this.currentRound % 3 === 0){
+                return false;
+            }
+            return true;
         }
+
 
     },
     methods: {
         attackMonster(){
+            this.currentRound++;
             const attackValue =  getRandomValue(5,12)
             this.monsterHealth -= attackValue;
             this.attackPlayer();
@@ -27,6 +36,12 @@ const app = Vue.createApp({
         attackPlayer(){
             const attackValue =  getRandomValue(8,15)
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound++;
+            const attackValue =  getRandomValue(10,25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 
